28e2dc88db227938297d1e15aeb73f08b7051edf
Tasnim Faizah <tee.faizah@gmail.com>
2025-09-25 23:13:08 +0600

    Add files via upload

---
+++ Beginner_334/consumption.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    double y, dist;
+    cin >> x >> y;
+    dist = x/y;
+    cout << fixed <<setprecision(3) << dist << " km/l" << endl;
+    return 0;
+}
